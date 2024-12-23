376599aab0a71987de5ca0f49e068210a851ac25
Manav-Rangpadiya <165824971+Manav-Rangpadiya@users.noreply.github.com>
2024-12-23 19:34:45 +0000

    js work

---
+++ basic/num.js
@@ -0,0 +1,22 @@
+// let num = 123.5
+// console.log(num.toFixed(0));
+// console.log(num.toExponential());
+
+// console.log(num.toPrecision(3))
+// let num1 = 11113090999
+// console.log(num1.toLocaleString('en-In'))
+
+
+//maths
+// let num1 = 138430.5435;
+// console.log(Math.floor(num1))
+// console.log(Math.ceil(num1));
+
+// let arr =[4,5,8,3,6,9,3]
+// console.log(Math.max(arr));
+// console.log(Math.min(arr));
+// console.log(arr);
+
+
+console.log(Math.round(Math.random()))
+console.log(Math.floor(Math.random() * 10) +1)
---
+++ basic/string.js
@@ -0,0 +1,58 @@
+// //Trim slice substring uppercase length [0] Replace includes split
+
+// //trim
+// let string1 = "    hello i am manav    "
+// let string2 = string1.trim(2,5)
+// console.log(string1);
+// console.log(string2);
+
+// //slice
+// let name1 = "bhaluprasad chaurasiya"
+// let name2 = name1.slice(0,5)
+// console.log(name1);
+// console.log(name2);
+
+// //substring
+// let a = "jaihoo jaihuuu"
+// let b = a.substring(0,5)
+// console.log(a)
+// console.log(b);
+
+// //uppercase and length and [0]
+// let string3 = "we are the next gen of golden india we are proud of it"
+// let upperString = string1.toUpperCase()
+// console.log(string3)
+// console.log(upperString);
+
+// console.log(string3.length);
+// console.log(string1[53])
+
+// //includes
+// console.log(string3.includes('next'));
+// console.log(string3.includes('parasf'));
+
+// //split
+// let arr = string3.split("of")
+// console.log(arr);
+
+// //charat
+// console.log(string3.charAt(7));
+
+// //concat
+// let abc = "21 para"
+// let def = "sf special force"
+// let ad = abc.concat(def)
+// console.log(ad);
+
+//lastindexof
+let mam = "manav"
+console.log(mam.lastIndexOf("a"));
+
+let str = "Hello World";
+console.log(str.lastIndexOf("o"));
+
+// //replace
+// let string0 = "jai ho india"
+// let string9 = string0.replace("india","Bharat")
+// console.log(string0)
+// console.log(string9)
