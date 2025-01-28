36af07a057c3f8959798c700daafc7f9da9d0ee3
Manav Rangpadiya <165824971+Manav-Rangpadiya@users.noreply.github.com>
2025-01-28 22:23:51 +0530

    Add files via upload

---
+++ basic/array.js
@@ -0,0 +1,40 @@
+let array1 = [1,2,3,4,5,6]
+// array1.push(10)
+// console.log(array1)
+// console.log(array1.pop());
+// console.log(array1)
+//  array1.unshift(0)
+//  console.log(array1)
+
+//  array1.shift()
+//  console.log(array1)
+// console.log(array1.indexOf(6));
+// console.log(array1.includes(9));
+// console.log(array1.includes(6));
+
+// let sortedArray = [9,5,1,7,8,2,3,6,4]
+// sortedArray.sort()
+// let reversesort = sortedArray.reverse()
+// console.log(reversesort);
+
+//Joining the 2 arrays
+// let a = [100]
+// let b = [200]
+// let c = [300]
+// let abc =[...a,...b,...c]
+// console.log(abc)
+
+// let ab = a.concat(b)
+// console.log(ab);
+// let m =10
+// let n =20
+// let r =30
+// let mnr = Array.of(m,n,r)
+// console.log(mnr);
+
+let name = "manav"
+let marks = [100,200,250,100]
+console.log(Array.isArray(name))
+console.log(Array.isArray(marks));
+console.log(Array.from(name));
+
