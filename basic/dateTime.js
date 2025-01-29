14af45810b4873f476a0f6a2dcb569a6c52d9e40
Manav Rangpadiya <165824971+Manav-Rangpadiya@users.noreply.github.com>
2025-01-29 21:10:03 +0530

    Add files via upload

---
+++ basic/dateTime.js
@@ -0,0 +1,8 @@
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toDateString());
+console.log(myDate.toString());
+console.log(myDate.toUTCString());
+console.log(myDate.toLocaleString());
