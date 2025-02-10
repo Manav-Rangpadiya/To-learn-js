ce55bf1226ffb548eccdd9fe1be197006c5c5ad3
Manav Rangpadiya <165824971+Manav-Rangpadiya@users.noreply.github.com>
2025-02-10 13:49:33 +0530

    Add files via upload

---
+++ basic/datatypes.js
@@ -0,0 +1,59 @@
+let num = 12
+let name = "manav"
+let score ="33"
+let isOkay = true
+let isGood = 0
+
+
+// console.log(typeof score);
+// console.log(score);
+
+// let stringToNum = Number (score)
+// console.log(typeof stringToNum);
+// console.log(stringToNum);
+
+// let stringToNum = Number (name)
+// console.log(typeof stringToNum);
+// console.log(stringToNum);
+
+// let numToBool = Boolean(isGood)
+// let boolToNum = Number(isOkay)
+
+// console.table([numToBool,boolToNum,typeof numToBool, typeof boolToNum])
+
+// let spinner = ["ashwin", "kumble", "Harbhajan", "warne", "Murli", "Vettoeri"]
+// console.log(spinner);
+// console.log(typeof spinner);
+
+// let obj1 = {
+//     name:"Bhuvneshver Kumar",
+//     Role:"Mediaum pacer",
+//     wickets: 579
+// }
+// console.log(obj1)
+// console.log(typeof obj1);
+
+// let function1 = function(){
+//     console.log("21-Para Sf");
+// }
+// console.log(function1);
+// console.log(typeof function1);
+
+// console.log(1+2+4);
+// console.log("1"+3+4);
+// console.log(2+3+"1")
+ 
+// console.log(null>0)
+// console.log(null==0);
+// console.log(null>=0);
+
+// let a = 10
+// let b = a++
+// console.log("a is",a)
+// console.log("B is",b);
+
+
+let a = 10
+let b = ++a
+console.log("a is",a)
+console.log("B is",b);
---
+++ basic/functions.js
@@ -0,0 +1,58 @@
+////  lets try functions   ////
+
+// function sumNumber (num1, num2){
+//     console.log(num1+num2)
+// }
+
+// sumNumber(4, 8)
+
+function sumOfTwo (x,y) {
+    let result =  x +y
+    return result
+}
+ console.log(sumOfTwo(10,100))
+
+// function userLoggedIn (username) {
+//     if(username != undefined){
+//         return `Welcome to the universe ${username}`
+//     }
+//     return`Please enter the name plzzz`
+// }
+
+// console.log(userLoggedIn("Manav"));
+
+// function calculateCart (...item1){
+//     return item1
+// }
+
+// console.log(calculateCart(100,230,500,365,152,1002)); //// array ma avse 
+
+
+// let person1 = {
+//     name : "manav",
+//     age  : 19,
+//     score : 200
+// }
+
+// function scoreCard (anyobject){
+//     return `Name is ${anyobject.name} and the legendry score is ${anyobject.score}`
+// }
+
+// console.log(scoreCard({
+//     name : "Manav Patel",
+//     score : 265
+// }));
+
+// let array1 = [100,200,3000,4000,500,600]
+
+// function minOfArray (arr){
+//     return`Minimum Value is ${Math.min(arr)}`
+// }
+
+// console.log(minOfArray(array1));
+
+//let array2 = [1,2,3,6,4,5]
+// function secondValue (getarr) {
+//     return `second valuve is ${getarr[1]}`
+// }
+// console.log(secondValue([2,8,5,4]));
