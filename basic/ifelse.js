7196b1a85e396a459f93e6f9bebaa7b36a941eec
Manav Rangpadiya <165824971+Manav-Rangpadiya@users.noreply.github.com>
2025-02-13 10:27:40 +0530

    Add files via upload

---
+++ basic/ifelse.js
@@ -0,0 +1,47 @@
+let a = 46
+let b = 58
+let c = 59
+
+if (a>b) {
+    if (a>c) {
+        console.log("a is big");
+    }    
+}
+else if (b>a) {
+    if (b > c) {
+        console.log("b is big");
+    }
+}
+else{
+    console.log("C is big");
+}
+
+// if ( a > b && a >c ) {
+
+//     console.log(`a is big`);
+    
+// }
+// else if ( b > a && b > c) {
+
+//     console.log(`b is big`);
+    
+// }
+// else{
+
+//     console.log(`c is big`);
+// }
+
+// let value = 1550
+
+// if (value < 900) {
+//     console.log("value less then 900");
+// }
+// else if (value < 1100) {
+//     console.log("value is less then 1100");
+// }
+// else if (value < 1300) {
+//     console.log("value is less then 1300");
+// }
+// else{
+//     console.log("value is much much yumy");
+// }
