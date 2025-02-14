a2da1fc4b9cff47015481263bb8a7f08bc7b15ee
Manav Rangpadiya <165824971+Manav-Rangpadiya@users.noreply.github.com>
2025-02-14 23:38:11 +0530

    Add files via upload

---
+++ basic/mfr.js
@@ -0,0 +1,33 @@
+const number = [25,14,63,24,45,86,95,12]
+
+// let newnum = number.filter( (i) => i > 50 )
+// console.log(newnum);
+
+
+// let total =number.reduce( (i) => i=i+i )
+// console.log(total);
+
+// const books = [
+//     {book :"book 1", genre :"history", publish : 1985},
+//     {book :"book 2", genre :"sciense", publish : 1963},
+//     {book :"book 3", genre :"novel", publish : 2005},
+//     {book :"book 4", genre :"history", publish : 1999},
+//     {book :"book 5", genre :"novel", publish : 1965},
+//     {book :"book 6", genre :"history", publish : 1995},
+//     {book :"book 7", genre :"history", publish : 2001}
+// ]
+
+// console.log(books);
+
+// let newhisbk = books.filter( (bk) => bk.genre === "history" && bk.publish>1990)
+// console.log(newhisbk);
+
+// let newNum = number.map( (i) => i = i +10 )
+// console.log(newNum);
+
+// let red = number.reduce( (item) => {
+//     item = item +item
+//     item++
+// } )
+// console.log(red);
+
