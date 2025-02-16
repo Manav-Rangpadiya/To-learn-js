bd75e2f0e95b3bf60217ccc811b030ef634e12e4
Manav Rangpadiya <165824971+Manav-Rangpadiya@users.noreply.github.com>
2025-02-16 16:01:27 +0530

    Add files via upload

---
+++ basic/thisArrow.js
@@ -0,0 +1,35 @@
+// let person1 = {
+//     username : "Manav",
+//     age : 19,
+
+//     greet : function grrretings (){
+//         console.log(`Jay Hind fauji ${this.username}bhai`);
+           // console.log(this);
+//     }
+// }
+//console.log(person1.greet());
+//person1.greet()
+// // console.log(this);
+// console.log(abc);
+// let abc = function xyz(){
+//     console.log(`jai ho`);
+// }
+
+
+// function manav () { 
+//     console.log(`kalo ke kal Mahakal`);
+// }
+// manav()
+
+// const abc = (x,y) => x+y
+// console.log(abc(10,20));
+
+////   iife  ////
+
+(function mandy () {
+    console.log(`21 para sf`);
+})();
+
+( () => {
+    console.log(`31 r. rifel`);
+} ) ()
